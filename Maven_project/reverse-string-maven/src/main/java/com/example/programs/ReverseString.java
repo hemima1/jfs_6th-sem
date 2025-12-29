5f9ec1830344d6fdf54fbb89af2677e9ec18bfda
Hemima Kothapalli <hemimakothapalli@gmail.com>
2025-12-29 09:54:10 +0530

    Add files via upload

---
+++ Maven_project/reverse-string-maven/src/main/java/com/example/programs/ReverseString.java
@@ -0,0 +1,15 @@
+package com.example.programs;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Original: " + str);
+        System.out.println("Reversed: " + reversed);
+    }
+}
